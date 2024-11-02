178fb1d9e28243afe41abc4a6ec9dae0af2ee53d
kinleyrabgay <fsd.rabgay@gmail.com>
2024-11-02 15:43:21 +0600

    Setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className='h1-bold'>Hello World, welcome to Next.js</h1>
-      <h1 className='h1-bold font-space-grotesk'>
-        Hello World, welcome to Next.js
-      </h1>
+      <h1 className='h1-bold text-center'>👋</h1>
     </div>
   );
 }
