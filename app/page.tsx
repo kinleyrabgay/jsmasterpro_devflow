d2dd969be2e809fd11368a3b8f0e02b3c4d0189b
kinleyrabgay <fsd.rabgay@gmail.com>
2024-11-02 15:36:16 +0600

    Font Setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <div>
       <h1 className='h1-bold'>Hello World, welcome to Next.js</h1>
+      <h1 className='h1-bold font-space-grotesk'>
+        Hello World, welcome to Next.js
+      </h1>
     </div>
   );
 }
