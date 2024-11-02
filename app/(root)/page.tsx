9309e2f672e33f7e12302d576f8fbe9fd2e3a66a
kinleyrabgay <fsd.rabgay@gmail.com>
2024-11-02 18:11:46 +0600

    Left and Right Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,7 @@
-import { auth, signOut } from '@/auth';
-import { Button } from '@/components/ui/button';
-import ROUTES from '@/constants/routes';
+import React from 'react';
 
-const Home = async () => {
-  const session = await auth();
-
-  console.log(session);
-
-  return (
-    <>
-      <h1 className='h1-bold'>Welcome to the world of Next.js</h1>
-
-      <form
-        className='px-10 pt-[100px]'
-        action={async () => {
-          'use server';
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type='submit'>Log out</Button>
-      </form>
-    </>
-  );
+const Home = () => {
+  return <div>Home</div>;
 };
 
 export default Home;
